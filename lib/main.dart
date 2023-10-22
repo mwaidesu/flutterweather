3223190b49f5d079ee8d46355054d3cb0af321f2
mwaidesu <machariamwai@gmail.com>
2023-10-22 22:48:45 +0300

    Add Google Fonts(Poppins) as default text theme

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:weatherapp/screens/home_screen.dart';
 
 import 'bloc/weather_bloc_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -16,10 +17,20 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+        theme: _buildTheme(Brightness.dark),
         debugShowCheckedModeBanner: false,
+        
         home: BlocProvider<WeatherBlocBloc>(
           create: (context) => WeatherBlocBloc()..add(FetchWeather()),
           child: HomeScreen(),
         ));
   }
+}
+
+ThemeData _buildTheme(brightness) {
+  var baseTheme = ThemeData(brightness: brightness);
+
+  return baseTheme.copyWith(
+    textTheme: GoogleFonts.poppinsTextTheme(baseTheme.textTheme),
+  );
 }
